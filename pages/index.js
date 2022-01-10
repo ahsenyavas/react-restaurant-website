8cd466cbde5dbfc4ba63b1a5094d3f267afbf292
Ahsen Yavas <ahsen.yavas@goatworks.io>
2022-01-10 13:51:54 +0300

    init

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pizza Restaurant</title>
+        <meta name="description" content="restaurant" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+     
+      homepage
+      <Image src="/image/pizza-logo.png" alt="logo" width={30} height={30}/>
+     
+    </div>
+  );
+}
